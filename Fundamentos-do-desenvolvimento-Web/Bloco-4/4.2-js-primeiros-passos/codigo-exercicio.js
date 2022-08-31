e09b8547aded42fdf379e9923bfafe885ca64928
Claudio <chagasclaudio4@gmail.com>
2022-08-31 01:30:42 -0300

    bloco 10

---
+++ Fundamentos-do-desenvolvimento-Web/Bloco-4/4.2-js-primeiros-passos/codigo-exercicio.js
@@ -0,0 +1,31 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//console.log(numbers);
+
+let result = 0;
+
+
+
+// for(let sum of numbers){ //ex 1
+//  result = result+sum;
+// console.log(result);
+// }
+
+//Exercicio 3 e 4
+
+// for(let sum of numbers){ //ex 1
+//       result = result+sum;
+
+//      console.log(result/numbers.length);  
+
+//     if ((result/numbers.length)>20 ){
+//         console.log('valor maior que 20')
+//     }else{
+//         console.log('valor menor que 20')
+//     }
+// }
+let tamanho_array=numbers.length;
+
+for(index = 1; numbers.length > tamanho_array; index +=1) {
+    
+
+}
